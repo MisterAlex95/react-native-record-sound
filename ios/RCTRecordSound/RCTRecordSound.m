3a0e8ed619686ca40f11c16eaecaa0c8e05fbc22
MisterAlex95 <alex.dana@epitech.eu>
2016-06-09 13:24:26 +0200

    * Fix: use NSString instead of NSURL

--- ios/RCTRecordSound/RCTRecordSound.m
+++ ios/RCTRecordSound/RCTRecordSound.m
@@ -21,7 +21,7 @@
 RCT_EXPORT_MODULE();
 
 
-RCT_EXPORT_METHOD(startRecord:(NSURL *)filename : (RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(startRecord:(NSString *)filename : (RCTResponseSenderBlock)callback) {
     NSNumber *audioQuality = [NSNumber numberWithInt:AVAudioQualityHigh];
     recordSettings = [NSDictionary dictionaryWithObjectsAndKeys:
                       audioQuality, AVEncoderAudioQualityKey,
@@ -30,22 +30,22 @@ RCT_EXPORT_METHOD(startRecord:(NSURL *)filename : (RCTResponseSenderBlock)callba
                       16000.0, AVSampleRateKey,
                       nil];
     NSError* error = nil;
-    
+
     AVAudioSession* _recordSession = [AVAudioSession sharedInstance];
     [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord
                         error:nil];
-    
+
     _audioRecord = [[AVAudioRecorder alloc]
                     initWithURL: filename
                     settings:recordSettings
                     error: &error];
-    
+
     if (error) {
         ;
     } else {
         [_audioRecord prepareToRecord];
     }
-    
+
     if (!_audioRecord.recording) {
         [_audioRecord record];
     }
