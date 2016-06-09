cf577ea4b0080c0fc907c2108e4d4472cdb1323d
Alex Dana <Alex@Alexs-Mac.local>
2016-06-09 13:00:05 +0200

    Add ios version

--- ios/RCTRecordSound/RCTRecordSound.h
+++ ios/RCTRecordSound/RCTRecordSound.h
@@ -1,10 +1,16 @@
+//
+//  RCTRecordSound.h
+//  RCTRecordSound
+//
+//  Created by Alex Dana on 08/06/16.
+//  Copyright © 2016 Alex Dana. All rights reserved.
+//
+
 // RecordSound.h
 // add AVFoundation Framework in Xcode
 
-#import "RCTBridgeModule.h"
+#import "RCTBridge.h"
 #import <AVFoundation/AVFoundation.h>
 
 @interface ReactNativeRecordSound : NSObject <RCTBridgeModule ,AVAudioRecorderDelegate>
-
-
 @end
---
+++ ios/RCTRecordSound/RCTRecordSound.m
@@ -0,0 +1,60 @@
+//
+//  RCTRecordSound.m
+//  RCTRecordSound
+//
+//  Created by Alex Dana on 08/06/16.
+//  Copyright © 2016 Alex Dana. All rights reserved.
+//
+
+#import "RCTRecordSound.h"
+#import "RCTConvert.h"
+#import "RCTBridge.h"
+#import "RCTEventDispatcher.h"
+#import <AVFoundation/AVFoundation.h>
+
+@implementation ReactNativeRecordSound {
+    NSDictionary * recordSettings;
+    AVAudioSession *audioSession;
+    AVAudioRecorder* _audioRecord;
+}
+
+RCT_EXPORT_MODULE();
+
+
+RCT_EXPORT_METHOD(startRecord:(NSURL *)filename : (RCTResponseSenderBlock)callback) {
+    NSNumber *audioQuality = [NSNumber numberWithInt:AVAudioQualityHigh];
+    recordSettings = [NSDictionary dictionaryWithObjectsAndKeys:
+                      audioQuality, AVEncoderAudioQualityKey,
+                      kAudioFormatMPEGLayer3, AVFormatIDKey,
+                      1, AVNumberOfChannelsKey,
+                      16000.0, AVSampleRateKey,
+                      nil];
+    NSError* error = nil;
+    
+    AVAudioSession* _recordSession = [AVAudioSession sharedInstance];
+    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord
+                        error:nil];
+    
+    _audioRecord = [[AVAudioRecorder alloc]
+                    initWithURL: filename
+                    settings:recordSettings
+                    error: &error];
+    
+    if (error) {
+        ;
+    } else {
+        [_audioRecord prepareToRecord];
+    }
+    
+    if (!_audioRecord.recording) {
+        [_audioRecord record];
+    }
+}
+
+RCT_EXPORT_METHOD(stopRecord) {
+    if (_audioRecord.recording) {
+      [_audioRecord stop];
+    }
+}
+
+@end
