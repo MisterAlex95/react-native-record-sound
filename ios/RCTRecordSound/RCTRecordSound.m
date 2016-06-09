805128f02fda0d35581d05e61513e85e811cebda
Alex Dana <Alex@Alexs-Mac.local>
2016-06-09 15:29:29 +0200

    fix ios

--- ios/RCTRecordSound/RCTRecordSound.m
+++ ios/RCTRecordSound/RCTRecordSound.m
@@ -22,30 +22,35 @@ RCT_EXPORT_MODULE();
 
 
 RCT_EXPORT_METHOD(startRecord:(NSURL *)filename : (RCTResponseSenderBlock)callback) {
-    NSNumber *audioQuality = [NSNumber numberWithInt:AVAudioQualityHigh];
-    recordSettings = [NSDictionary dictionaryWithObjectsAndKeys:
-                      audioQuality, AVEncoderAudioQualityKey,
-                      kAudioFormatMPEGLayer3, AVFormatIDKey,
-                      1, AVNumberOfChannelsKey,
-                      16000.0, AVSampleRateKey,
-                      nil];
-    NSError* error = nil;
+    NSNumber *_audioEncoding = [NSNumber numberWithInt:kAudioFormatMPEGLayer3];
+    NSNumber *_audioChannels = [NSNumber numberWithInt:1];
+    NSNumber *_audioSampleRate = [NSNumber numberWithFloat:16000.0];
+    NSNumber *_audioQuality = [NSNumber numberWithInt:AVAudioQualityHigh];
     
+    recordSettings = [NSDictionary dictionaryWithObjectsAndKeys:
+                      _audioQuality, AVEncoderAudioQualityKey,
+                      _audioEncoding, AVFormatIDKey,
+                      _audioChannels, AVNumberOfChannelsKey,
+                      _audioSampleRate, AVSampleRateKey,
+                      nil];
+    
+    NSError* error = nil;
+
     AVAudioSession* _recordSession = [AVAudioSession sharedInstance];
     [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord
                         error:nil];
-    
+
     _audioRecord = [[AVAudioRecorder alloc]
                     initWithURL: filename
                     settings:recordSettings
                     error: &error];
-    
+
     if (error) {
         ;
     } else {
         [_audioRecord prepareToRecord];
     }
-    
+
     if (!_audioRecord.recording) {
         [_audioRecord record];
     }
