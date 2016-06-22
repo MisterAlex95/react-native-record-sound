98812aded3128b36f5ef3551c4243a3b10cb6846
Alex Dana <Alex@Alexs-Mac.local>
2016-06-22 16:57:50 +0200

    IOS FIX again

--- ios/RCTRecordSound/RCTRecordSound.m
+++ ios/RCTRecordSound/RCTRecordSound.m
@@ -13,8 +13,8 @@
 #import <AVFoundation/AVFoundation.h>
 
 @implementation ReactNativeRecordSound {
-    NSDictionary * recordSettings;
-    AVAudioSession *audioSession;
+    NSDictionary* recordSettings;
+    AVAudioSession* audioSession;
     AVAudioRecorder* _audioRecord;
 }
 
@@ -42,16 +42,18 @@ RCT_EXPORT_METHOD(startRecord:(NSString *)filename : (RCTResponseSenderBlock)cal
     NSNumber *_audioEncoding = [NSNumber numberWithInt:kAudioFormatMPEG4AAC];
     NSNumber *_audioChannels = [NSNumber numberWithInt:1];
     NSNumber *_audioSampleRate = [NSNumber numberWithFloat:16000.0];
-    NSNumber *_audioQuality = [NSNumber numberWithInt:AVAudioQualityMax];
+    NSNumber *_audioQuality = [NSNumber numberWithInt:AVAudioQualityMin];
+//    NSNumber *_audioBiteRate = [NSNumber numberWithInt:16];
 
     recordSettings = [NSDictionary dictionaryWithObjectsAndKeys:
                       _audioQuality, AVEncoderAudioQualityKey,
                       _audioEncoding, AVFormatIDKey,
+//                      _audioBiteRate, AVEncoderBitRateKey,
                       _audioChannels, AVNumberOfChannelsKey,
                       _audioSampleRate, AVSampleRateKey,
                       nil];
 
-    NSError* error = nil;
+    NSError* err = nil;
 
     AVAudioSession* _recordSession = [AVAudioSession sharedInstance];
     [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord
@@ -62,25 +64,25 @@ RCT_EXPORT_METHOD(startRecord:(NSString *)filename : (RCTResponseSenderBlock)cal
     _audioRecord = [[AVAudioRecorder alloc]
                     initWithURL: soundFileURL
                     settings:recordSettings
-                    error: &error];
+                    error: &err];
 
-    if (error) {
-      NSLog(@"Error: %@ %d %@", [error domain], [error code], [[error userInfo] description]);
-      return;
+    if (err) {
+        NSLog(@"audioSession: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+        return;
     } else {
         [_audioRecord prepareToRecord];
+        NSLog(@"audioSession: launch record");
     }
 
     if (_audioRecord.recording == false) {
         [_audioRecord record];
-        NSLog(@"AudioRecord: Begin record");
     }
 }
 
 RCT_EXPORT_METHOD(stopRecord) {
     if (_audioRecord.recording == true) {
-      NSLog(@"AudioRecord: Stop record");
-      [_audioRecord stop];
+        NSLog(@"audioSession: stop record");
+        [_audioRecord stop];
     }
 }
 
